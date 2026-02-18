a41337a3873ab117140fc0e01c4f1c58d04db7f1
Artem Levchenko <levchencode@gmail.com>
2026-02-18 15:12:19 +0200

    database

---
+++ internal/infra/database/mongo.go
@@ -0,0 +1,57 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/rs/zerolog"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+
+	"recipes-desk/internal/config"
+)
+
+type MongoDB struct {
+	Client   *mongo.Client
+	Database *mongo.Database
+}
+
+func New(cfg config.MongoDBConfig, log *zerolog.Logger) (*MongoDB, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	clientOptions := options.Client().ApplyURI(cfg.URI)
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to MongoDB: %w", err)
+	}
+
+	ctxPing, cancelPing := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelPing()
+
+	if err = client.Ping(ctxPing, readpref.Primary()); err != nil {
+		return nil, fmt.Errorf("failed to ping MongoDB: %w", err)
+	}
+
+	db := client.Database(cfg.Database)
+
+	log.Info().Msg("Connected to MongoDB")
+
+	return &MongoDB{
+		Client:   client,
+		Database: db,
+	}, nil
+}
+
+func (m *MongoDB) Disconnect(ctx context.Context) error {
+	return m.Client.Disconnect(ctx)
+}
+
+func (m *MongoDB) Health(ctx context.Context) error {
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+	return m.Client.Ping(ctx, readpref.Primary())
+}
