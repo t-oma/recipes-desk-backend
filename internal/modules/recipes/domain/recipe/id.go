789abfd4719fb77be6a92c373d3ef983c31fbf08
Artem Levchenko <levchencode@gmail.com>
2026-03-04 01:16:13 +0200

    id value object

---
+++ internal/modules/recipes/domain/recipe/id.go
@@ -0,0 +1,34 @@
+package recipe
+
+import (
+	"fmt"
+	"strings"
+)
+
+type EntityID struct {
+	value string
+}
+
+func NewEntityID(id string) (EntityID, error) {
+	id = strings.TrimSpace(id)
+	if id == "" {
+		return EntityID{}, ErrRecipeIDEmpty
+	}
+
+	return EntityID{
+		value: id,
+	}, nil
+}
+
+func (r EntityID) Value() string {
+	return r.value
+}
+
+func (r EntityID) String() string {
+	return r.value
+}
+
+var ErrRecipeIDEmpty = fmt.Errorf(
+	"%w: recipe ID cannot be empty",
+	ErrValidation,
+)
---
+++ internal/modules/recipes/domain/recipe/id_test.go
@@ -0,0 +1,47 @@
+package recipe_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"recipes-desk/internal/modules/recipes/domain/recipe"
+	"recipes-desk/pkg/stringutil"
+)
+
+func TestNewRecipeID(t *testing.T) {
+	tests := []struct {
+		name    string
+		id      string
+		wantErr error
+	}{
+		{
+			name:    "valid id",
+			id:      stringutil.RandomString(10),
+			wantErr: nil,
+		},
+		{
+			name:    "empty id",
+			id:      "",
+			wantErr: recipe.ErrRecipeIDEmpty,
+		},
+		{
+			name:    "only whitespaces",
+			id:      "         ",
+			wantErr: recipe.ErrRecipeIDEmpty,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, gotErr := recipe.NewEntityID(tt.id)
+			if tt.wantErr != nil {
+				require.Error(t, gotErr)
+				require.ErrorIs(t, gotErr, tt.wantErr)
+			} else {
+				require.NoError(t, gotErr)
+				require.NotEmpty(t, got.String())
+				require.NotEmpty(t, got.Value())
+			}
+		})
+	}
+}
