5a0d469aacfd3c87abfa0654077f0bfd322ee3e8
Artem Levchenko <levchencode@gmail.com>
2026-03-12 23:13:04 +0200

    renamed

--- pkg/config/yaml.go
+++ pkg/config/yaml.go
@@ -1,10 +1,16 @@
 package config
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
 	"gopkg.in/yaml.v3"
+)
+
+var (
+	ErrReadFile  = errors.New("read config file")
+	ErrUnmarshal = errors.New("unmarshal yaml data")
 )
 
 // LoadYAML loads a YAML configuration file.
@@ -12,12 +18,12 @@ import (
 func LoadYAML[T any](filename string) (*T, error) {
 	data, err := os.ReadFile(filename)
 	if err != nil {
-		return nil, fmt.Errorf("failed to read config file: %w", err)
+		return nil, fmt.Errorf("%w: %w", ErrReadFile, err)
 	}
 
 	var config T
 	if err = yaml.Unmarshal(data, &config); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal yaml data: %w", err)
+		return nil, fmt.Errorf("%w: %w", ErrUnmarshal, err)
 	}
 
 	return &config, nil
